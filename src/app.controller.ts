f46dc6c48450da2505f0c10bbdadd94ffbab5dd4
Adeel Shabbir <madeelshabbirs@gmail.com>
2023-11-18 16:31:35 +0500

    Routing Decorators

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,14 @@
 import { Controller, Get } from '@nestjs/common';
 
-@Controller()
+@Controller('/app')
 export class AppController {
   @Get()
   getRootRoute() {
-    return 'Hi there!s';
+    return 'Hi there!';
+  }
+
+  @Get('/bye')
+  getByeRoute() {
+    return 'Bye there!';
   }
 }
